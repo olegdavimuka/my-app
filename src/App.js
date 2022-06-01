d7498f8c964e9c9c1ffcc9cac4baf0048576d4b8
Oleh Davymuka <olegdavimuka0@gmail.com>
2022-06-01 18:42:31 +0300

    Lesson 49. Add users page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
+import UsersContainer from './components/Users/UsersContainer'
 
 
 const App = (props) => {
@@ -15,6 +16,7 @@ const App = (props) => {
         <Routes>
           <Route path='/dialogs' element={<DialogsContainer />} />
           <Route path='/profile' element={<Profile />} />
+          <Route path='/users' element={<UsersContainer />} />
         </Routes>
       </div>
     </div>
